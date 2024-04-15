dd43c8ce20b6a6b94bf4f0a670409db7cb1522ed
khalid-sohaib <khalid.shoaib@datics.ai>
2024-04-15 10:16:00 +0500

    windows iniatl commit

---
+++ components/BgImage.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function BgImage() {
+  return (
+    <View>
+      <Text>BgImage</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
